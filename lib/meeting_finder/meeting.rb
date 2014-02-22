b931f25e222c05126389e9106d2ed32a3592f857
Ben Lewis <bennlewis@gmail.com>
2014-02-22 14:49:59 -0700

    remove fellowship

--- lib/meeting_finder/meeting.rb
+++ lib/meeting_finder/meeting.rb
@@ -1,13 +1,12 @@
 module MeetingFinder
   class Meeting
-    attr_reader :name, :location, :address, :day, :time, :fellowship, :lat, :lng
+    attr_reader :name, :location, :address, :day, :time, :lat, :lng
     def initialize(attributes={})
       @name = attributes['name']
       @location = attributes['location']
       @address = attributes['address']
       @day = attributes['day']
       @time = attributes['time']
-      @fellowship = attributes['fellowship']
       @lat = attributes['lat']
       @lng = attributes['lng']
     end
